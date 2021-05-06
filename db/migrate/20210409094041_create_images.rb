42021c399b47a9bc7be656f1716c4ab5893ce2ed
= <=>
2021-05-06 18:16:22 +0800

    Fix error for references

--- db/migrate/20210409094041_create_images.rb
+++ db/migrate/20210409094041_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[6.1]
   def change
     create_table :images do |t|
       t.integer :type, null:false
-      t.references :imageable, polymorphic: true, null: false
+      t.references :imageable, polymorphic: true
 
       t.timestamps
     end
--- db/migrate/20210419135355_create_categories.rb
+++ db/migrate/20210419135355_create_categories.rb
@@ -3,8 +3,7 @@ class CreateCategories < ActiveRecord::Migration[6.1]
     create_table :categories do |t|
       t.string :title, null: false
       t.timestamps
+      t.references :parent, foreign_key: { to_table: :categories }
     end
-
-    t.references :parent, foreign_key: { to_table: :categories }
   end
 end
