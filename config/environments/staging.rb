d13399d0e80391ef2af40fd6a357411447628bcb
= <=>
2021-05-10 18:15:49 +0800

    Add staging host url

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -125,5 +125,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.hosts << "kwartracker-staging.herokuapp.com"
+  config.hosts << "kwartracker-api-staging-76deg.ondigitalocean.app"
 end
