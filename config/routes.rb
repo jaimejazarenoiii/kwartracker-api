91a0273a4eedd03e319f0d4b7c1390b34441b122
= <=>
2021-05-11 17:44:44 +0800

    Change logic in route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' unless Rails.env.production?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development? || Rails.env.staging?
   post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
