f6710ab27184fbb06e5cfff69c58a61e58149871
= <=>
2021-05-11 20:26:52 +0800

    Define host from env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.hosts << "kwartracker-api-staging-76deg.ondigitalocean.app"
+  config.action_controller.default_url_options = { host: ENV['APPLICATION_HOST'] }
   config.serve_static_assets = true
   config.public_file_server.enabled = true
 end
