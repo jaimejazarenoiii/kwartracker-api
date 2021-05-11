df043d03a50f10e2e2fd7672fb965abb8205d6b7
= <=>
2021-05-11 17:18:37 +0800

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: "home#index"
   devise_for :users
-  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development? || Rails.env.staging?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' unless Rails.env.production?
   post '/graphql', to: 'graphql#execute'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
