a48350f1501dcb1ee8a633be33421201c66bf7bf
= <=>
2021-05-13 12:45:51 +0800

    Add creted and updated at field in budget

--- app/graphql/types/payloads/budget_type.rb
+++ app/graphql/types/payloads/budget_type.rb
@@ -8,6 +8,8 @@ module Types
       field :id, ID, null: false
       field :amount, Float, null: true
       field :category, Types::Payloads::CategoryType, null: false
+      field :updated_at, GraphQL::Types::ISO8601Date, null: false
+      field :created_at, GraphQL::Types::ISO8601Date, null: false
     end
   end
 end
